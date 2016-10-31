476e15dcc1d92f65a7d89fd1cec7d8d61908fdda
zthxxx <zth_9451@qq.com>
2016-10-31 11:17:43 +0800

    Try to fix travis and rename script

--- travis_env_init.sh
+++ travis_env_init.sh
@@ -42,9 +42,8 @@ sed -i'' "s~duoshuo: ~duoshuo: ${DuoShuo_SHORT_NAME} ~" "${theme_config_file}"
 if [ ! ${icarus_opacity_disable} ]
 then
     echo "Enable icarus opacity version."
-    chmod +x ./rename_BGI.sh
     ls -al
-    background_images_count=$($(pwd)/rename_BGI.sh)
+    background_images_count=$(source rename_BGI.sh)
     if [ $? -eq 0 ]
     then
         sed -i'' "s~enable: false # Is choose the opacity version of this theme~enable: true # ~" "${theme_config_file}"
