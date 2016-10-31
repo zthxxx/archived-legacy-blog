63e8caa7cd56b20736d37bce44ba11c4853fd7f1
zthxxx <zth_9451@qq.com>
2016-10-31 10:03:19 +0800

    Try to put background images in order by shell script.

--- travis_env_init.sh
+++ travis_env_init.sh
@@ -27,8 +27,19 @@ git config --global user.name "${GIT_USER_NAME}"
 git config --global user.email "${GIT_USER_EMAIL}"
 sed -i'' "s~git@github.com:~https://${GIT_REPO_TOKEN}@github.com/~" _config.yml
 
-cp themes/icarus/_config.yml.example themes/icarus/_config.yml
-sed -i'' "s~baidu_analytics: ~baidu_analytics: ${BAIDU_ANALYTICS} ~" themes/icarus/_config.yml
-sed -i'' "s~duoshuo: ~duoshuo: ${DuoShuo_SHORT_NAME} ~" themes/icarus/_config.yml
+theme_config_file="themes/icarus/_config.yml"
+cp "${theme_config_file}.example" "${theme_config_file}"
+# Set icarus theme config
+# Set baidu statistics open token
+sed -i'' "s~baidu_analytics: ~baidu_analytics: ${BAIDU_ANALYTICS} ~" "${theme_config_file}"
+# Set duoShuo commentary short name
+sed -i'' "s~duoshuo: ~duoshuo: ${DuoShuo_SHORT_NAME} ~" "${theme_config_file}"
+# Set icarus theme opacity version config
+sed -i'' "s~enable: false # Is choose the opacity version of this theme~enable: true # ~" "${theme_config_file}"
+background_images_count=$(sh rename_BGI.sh)
+sed -i'' "s~background_images_count: 0 ~background_images_count: ${background_images_count} ~" "${theme_config_file}"
+
+ls -al source/images/background/
+cat "theme_config_file"
 
 echo "Hexo environment pre install complete OK."
