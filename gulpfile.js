fe9d94025f82c0ba09d6942f0f93bfdd33eb7e3a
zthxxx <zth_9451@qq.com>
2017-02-02 15:09:31 +0800

    Fixed gulp imagemin option, make it support Progressive JPEG

--- gulpfile.js
+++ gulpfile.js
@@ -13,29 +13,34 @@ gulp.task('minify-css', function() {
 });
 // 压缩 public 目录 html
 gulp.task('minify-html', function() {
-  return gulp.src('./public/**/*.html')
-    .pipe(htmlclean())
-    .pipe(htmlmin({
-         removeComments: true,
-         minifyJS: true,
-         minifyCSS: true,
-         minifyURLs: true,
-    }))
-    .pipe(gulp.dest('./public'))
+    return gulp.src('./public/**/*.html')
+        .pipe(htmlclean())
+        .pipe(htmlmin({
+             removeComments: true,
+             minifyJS: true,
+             minifyCSS: true,
+             minifyURLs: true,
+        }))
+        .pipe(gulp.dest('./public'))
 });
 // 压缩 public 目录 js
 gulp.task('minify-js', function() {
-  return gulp.src('./public/**/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('./public'));
+    return gulp.src('./public/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./public'));
 });
 // 压缩图片任务
 gulp.task('minify-imgs', function () {
-  return gulp.src('./public/images/**/*.*')
-    .pipe(imagemin({
-        progressive: true
-    }))
-    .pipe(gulp.dest('./public/images'))
+    return gulp.src('./public/images/**/*.*')
+        // imagemin Usage at https://github.com/sindresorhus/gulp-imagemin#user-content-options
+        .pipe(imagemin([
+                imagemin.gifsicle(), 
+                imagemin.jpegtran({progressive: true}), 
+                imagemin.optipng(), 
+                imagemin.svgo()
+            ], {verbose: false}
+        ))
+        .pipe(gulp.dest('./public/images'))
 });
 // 执行 gulp 命令时执行的任务
 gulp.task('default', [
